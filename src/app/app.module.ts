7d816ce5601d16700d0334978a613d72bc8c013c
arturo villa <afve_1992@hotmail.com>
2019-11-07 20:11:31 -0500

    formularios reactivos con api autores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { TemplateComponent } from './template/template.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app.routing';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthorService } from './services/author.service';
 
 @NgModule({
   declarations: [
@@ -15,10 +16,11 @@ import { AppRoutingModule } from './app.routing';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthorService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/biblioteca/autores/autores-form/autores-form.component.ts
+++ src/app/pages/biblioteca/autores/autores-form/autores-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthorService } from 'src/app/services/author.service';
 
 @Component({
   selector: 'app-autores-form',
@@ -8,12 +9,18 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 })
 export class AutoresFormComponent implements OnInit {
 
-  constructor(protected fb: FormBuilder ) { }
-
+  constructor(protected fb: FormBuilder, protected service:AuthorService ) { }
+  authors : any;
   public autoresformForm: FormGroup;
   public titulo="formulario de autores";
-
+  
   ngOnInit() {
+    this.service.getAuthors().subscribe( data=>{
+      this.authors=data
+      
+      });
+
+
   }
  createForm(){
     this.autoresformForm = this.fb.group({
@@ -25,4 +32,20 @@ export class AutoresFormComponent implements OnInit {
       bd_place: ['', Validators.required]
       }) 
     }
+
+  Saveform(){
+    
+  this.service.postAuthor(this.autoresformForm.value).subscribe( data =>
+  alert('listo')
+  
+  );
+
 }
+
+
+
+}
+
+
+
+
